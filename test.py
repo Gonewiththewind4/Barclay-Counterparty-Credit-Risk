20684a13e6fd2bb7ceb1ebff351ec2d580d4b9ec
Gonewiththewind4 <523443113@qq.com>
2019-04-20 17:23:13 +0100

    update for travis,yml

---
+++ test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 21 00:29:13 2019
+
+@author: Gonewiththewind
+"""
+
+from SimulationMonteCarlo.code import FXForward
+
+FXForward._init_(FXForward,'GBP', 'GBP', 800, 800, '2014-10-24')
+print(FXForward._repr_(FXForward))
+print(FXForward.pricingTheTrade(FXForward,1))
